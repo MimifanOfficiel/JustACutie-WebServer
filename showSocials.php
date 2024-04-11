e65c25f5c3e8ec1054e0cbdf0c18fbf07ef33e6f
Mimifan <mimifan.dev@gmail.com>
2024-04-12 00:43:44 +0200

    .

--- showSocials.php
+++ showSocials.php
@@ -1,29 +1,47 @@
 <?php
 
-$database = new SQLite3('passwords.db');
+$db = new SQLite3('passwords.db');
 
-$results = $database->query('SELECT * FROM accounts');
-echo $results;
 
-echo "<table border='1'>
-        <tr>
-            <th>id</th>
-            <th>username</th>
-            <th>password</th>
-            <th>social</th>
-        </tr>";
+$query = "SELECT * FROM accounts";
 
-while ($row = $results->fetchArray()) {
-    echo "<tr>";
-    echo "<td>" . $row['id'] . "</td>";
-    echo "<td>" . $row['username'] . "</td>";
-    echo "<td>" . $row['password'] . "</td>";
-    echo "<td>" . $row['social'] . "</td>";
-    echo "</tr>";
+// Exécution de la requête
+$result = $db->query($query);
+
+// Vérification des erreurs
+if (!$result) {
+    die("Erreur lors de l'exécution de la requête : " . $db->lastErrorMsg());
 }
 
+// Affichage des résultats
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    // Affichage de chaque ligne
+    print_r($row);
+}
 
-echo "</table>";
+// Fermeture de la connexion à la base de données
+$db->close();
+
+// $results = $database->query('SELECT * FROM accounts');
+// echo "<table border='1'>
+//         <tr>
+//             <th>id</th>
+//             <th>username</th>
+//             <th>password</th>
+//             <th>social</th>
+//         </tr>";
+
+// while ($row = $results->fetchArray()) {
+//     echo "<tr>";
+//     echo "<td>" . $row['id'] . "</td>";
+//     echo "<td>" . $row['username'] . "</td>";
+//     echo "<td>" . $row['password'] . "</td>";
+//     echo "<td>" . $row['social'] . "</td>";
+//     echo "</tr>";
+// }
+
+
+// echo "</table>";
 
 
 ?>
