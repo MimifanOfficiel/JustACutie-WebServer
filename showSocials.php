c18aaefa8987e1b63436be4d0a007e7f713e7724
Mimifan <mimifan.dev@gmail.com>
2024-04-12 00:32:39 +0200

    little table

--- showSocials.php
+++ showSocials.php
@@ -1,5 +1,28 @@
 <?php
 
-echo "<p>Hello World! This is a PHP file.</p>";
+<?php
+
+$database = new SQLite3('passwords.db');
+
+$results = $database->query('SELECT * FROM accounts');
+
+echo "<table border='1'>
+<tr>
+<th>id</th>
+<th>username</th>
+<th>password</th>
+<th>social</th>
+</tr>";
+
+while ($row = $results->fetchArray()) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['username'] . "</td>";
+    echo "<td>" . $row['password'] . "</td>";
+    echo "<td>" . $row['social'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
 
 ?>
