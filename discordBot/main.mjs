ea10dfc6b63f867952215cd403bd1f397f02bf0e
Mimifan <mimifan.dev@gmail.com>
2024-04-12 18:13:03 +0200

    Duh

--- discordBot/main.mjs
+++ discordBot/main.mjs
@@ -15,11 +15,11 @@ export var guild;
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', async () => {
-    guild = await client.guilds.fetch("1228013193092141096").then(guild => {
-        const channel = client.channels.cache.get('1228013193708961935');
-        channel.send("Haiii, I'm online to assist Mommy :3");
-        return guild;
-    });
+    const channel = client.channels.cache.get('1228013193708961935');
+    channel.send("Haiii, I'm online to assist Mommy :3");
+    // guild = await client.guilds.fetch("1228013193092141096").then(guild => {
+        // return guild;
+    // });
 
 });
 
