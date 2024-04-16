2fb0c5a58e038784f6858ecc5a086de4432a8e9e
Mimifan <mimifan.dev@gmail.com>
2024-04-16 16:33:02 +0200

    Moves handler import

--- discordBot/main.mjs
+++ discordBot/main.mjs
@@ -1,7 +1,6 @@
 import Discord, { Events, REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 
-import { registerCommands, importCommands } from './commands/commandHandler/handler.mjs';
 
 dotenv.config();
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
@@ -14,6 +13,8 @@ export const client = new Discord.Client({
         Discord.IntentsBitField.Flags.GuildMembers
     ]
 });
+
+import { registerCommands, importCommands } from './commands/commandHandler/handler.mjs';
 
 export var guild;
 
